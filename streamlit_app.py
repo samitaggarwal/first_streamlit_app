b1dae68a8b0bf0722bb66ccc9abba4b94d766522
samitaggarwal <56025437+samitaggarwal@users.noreply.github.com>
2023-08-14 12:20:50 +0530

    error

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,4 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado',
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #streamlit.dataframe(my_fruit_list)
-streamlit.dataframe(fruits_selected)
+streamlit.dataframe(fruits_to_show)
