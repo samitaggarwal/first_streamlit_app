9b3264a124c0c2ffa738498586f5d4f9755e4efd
samitaggarwal <56025437+samitaggarwal@users.noreply.github.com>
2023-08-15 22:52:37 +0530

    qr

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ return fruityvice_normalized
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
 if not fruit_choice:
 streamlit.write('The user entered ', fruit_choice)
 else:
