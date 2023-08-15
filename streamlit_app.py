ea6f7295c46671d2f9af70b3f714143edbed4e70
samitaggarwal <56025437+samitaggarwal@users.noreply.github.com>
2023-08-15 22:44:31 +0530

    sfd

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,11 +33,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
+try:
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+if not fruit_choice:
 streamlit.write('The user entered ', fruit_choice)
-
-
+else:
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
@@ -45,7 +45,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-
+except URLError as e:
+    streamlit.error()
 streamlit.stop()
 
 
