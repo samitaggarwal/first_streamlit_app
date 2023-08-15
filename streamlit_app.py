39a52df544d3cedaaa449679101515157cc887c5
samitaggarwal <56025437+samitaggarwal@users.noreply.github.com>
2023-08-15 23:01:28 +0530

    saf

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,24 +32,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
-
-def get_fruityvice_data(this_fruit_choice):
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+if not fruit_choice:
+streamlit.write('Incorreect choice')
+else:
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-return fruityvice_normalized
-
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice:
-streamlit.write('Please select correct food')
-else:
-back_from_function= get_fruityvice_data(fruit_choice)
 # write your own comment - what does this do?
-streamlit.dataframe(back_from_function)
+streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
     streamlit.error()
 streamlit.stop()
